986bd7fcf1b59e92fd2a0fa4d2abede7e1019ea5
zracross <thinkariyan@gmail.com>
2026-07-12 10:20:03 +0530

    Initialize Firebase with configuration and exports

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Your Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyAloSW7mSLI11BWZ0wW0huXqUWHFFFwITQ",
+  authDomain: "zracross-chat.firebaseapp.com",
+  projectId: "zracross-chat",
+  storageBucket: "zracross-chat.firebasestorage.app",
+  messagingSenderId: "316090066115",
+  appId: "1:316090066115:web:c66bc8fc89fda0cdf8ee76"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
